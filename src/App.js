5266ab5876d9d00c50b541ef55064f7e578b78ef
frimsonkun <tokentanki@gmail.com>
2022-12-28 01:19:50 +0200

    Adding loguot button, pre-release version

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import { ChatEngine } from 'react-chat-engine';
+import { ChatEngine } from "react-chat-engine";
 
-import './App.css';
+import { useState, useEffect } from "react";
+import ChatFeed from "./components/ChatFeed";
+import LogOutButton from "./components/LogOut";
+import LoginForm from "./components/LoginForm";
+
+import "./App.css";
 
 const App = () => {
-    return ( <
-        ChatEngine height = "100vh"
-        projectID = "0c98ecf7-f2b9-4759-b3eb-e0f35ac45503"
-        userName = "dmytrodatsenko"
-        userSecret = "Dmytro"
+        const [isAuth, setIsAuth] = useState(localStorage.getItem("username"));
+        if (!isAuth) return <LoginForm isAuth = { isAuth }
+        setIsAuth = { setIsAuth }
+        />;
 
-        /
-        >
-    );
-}
+        return ( <
+            >
+            <
+            LogOutButton isAuth = { isAuth }
+            setIsAuth = { setIsAuth }
+            />{" "} <
+            ChatEngine height = "100vh"
+            projectID = "0c98ecf7-f2b9-4759-b3eb-e0f35ac45503"
+            userName = { localStorage.getItem("username") }
+            userSecret = { localStorage.getItem("password") }
+            renderChatFeed = {
+                (chatAppProps) => < ChatFeed {...chatAppProps }
+                />} /
+                >
+                <
+                />
+            );
+        };
 
-export default App;
+        export default App;
