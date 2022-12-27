729a0b104337387b06932f8fb2b05df77141b6d6
frimsonkun <tokentanki@gmail.com>
2022-12-27 18:45:33 +0200

    Creating a project and connecting ChatEngine

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import './App.css';
+
+const App = () => {
+    return ( <
+        ChatEngine height = "100vh"
+        projectID = "0c98ecf7-f2b9-4759-b3eb-e0f35ac45503"
+        userName = "dmytrodatsenko"
+        userSecret = "Dmytro"
+
+        /
+        >
+    );
+}
+
+export default App;
